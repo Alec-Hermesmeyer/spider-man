c37aec0fa34f82ed2e1a1f51fb3f8e4b4913f96a
Alec Hermessmeyer <alechermesmeyer@Alecs-MBP.lan>
2024-08-12 11:30:33 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+// public marvel key ="388a4de4d729dc268e39bf492ec7547e"
+//private marvel key = "7fb80ffb3b6d8547c7d7d9869ef449eada64dbed"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -28,15 +29,21 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full ">
+        {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src="/IMG_0092.JPG"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={280}
+          height={137}
           priority
-        />
+        /> */}
+      </div>
+      <div className="flex flex-col items-center justify-center text-center">
+        <input className="mb-5">
+        
+        </input>
+        <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
