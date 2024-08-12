95ff8a84030839bb52a4b6d455afddef8cb97f07
Alec Hermessmeyer <alechermesmeyer@Alecs-MBP.lan>
2024-08-12 14:51:44 -0500

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import Button from "./components/button";
 
 export default function Home() {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [loading, setLoading] = useState(false);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto lg:bg-none">
          
         </div>
       </div>
@@ -14,10 +20,15 @@ export default function Home() {
         
       </div>
       <div className="flex flex-col items-center justify-center text-center">
-        <input className="mb-5">
+        <input className="mb-5"
+        type="text"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        placeholder="Ask a question about Spider-Man..."
+        >
         
         </input>
-        <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+        <Button />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
